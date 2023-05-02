96cef102230a95351887fc636c7cfb4f13bf10ff
Ryu <ywg6525@daum.net>
2023-05-02 13:43:04 +0900

    [Feat/#1] add RotationView0

---
+++ MC2/View/RotationView/RotationView1.swift
@@ -0,0 +1,20 @@
+//
+//  RotationView1.swift
+//  MC2
+//
+//  Created by YU WONGEUN on 2023/05/02.
+//
+
+import SwiftUI
+
+struct RotationView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RotationView1_Previews: PreviewProvider {
+    static var previews: some View {
+        RotationView1()
+    }
+}
